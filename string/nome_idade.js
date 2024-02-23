bffe5eddb099f4c63fcd72389ef6ec80bbb5789f
MercioGiaretta <128699194+MercioGiaretta@users.noreply.github.com>
2024-02-23 11:57:52 -0300

    Add files via upload

--- string/nome_idade.js
+++ string/nome_idade.js
@@ -5,8 +5,16 @@
 let nome = "Márcio"
 let nome2 = "Giaretta"
 
-let inteiro = 17;
-let flutuante = 1.83;
+let altura = 1.83;
+let peso = 61;
 let flutuante2 = 24.07;
+let numero = 17;
+let inteiro = 7;
 
-console.log(`o nome do rapaiz é ${nome} sobrenome é ${nome2} e o ${nome} ${nome2} tem ${inteiro} anos, ${flutuante} de altura. E faz nasceu no dia ${flutuante2}`)
+console.log(`o nome do rapaiz é ${nome} sobrenome é ${nome2} e o ${nome} ${nome2} tem ${numero} anos, ${altura} de altura. E faz aniversario no dia ${flutuante2}`)
+console.log(`tem ${peso} e ${altura} de altura e se somar os dois juntos fica ${numero + inteiro}`)
+
+//Adicione uma variável com um número de 0 a 10,
+//E somem esse numero na idade da pessoa
+//Descrevam o peso e a altura da pessoa e imprimam no terminal
+
